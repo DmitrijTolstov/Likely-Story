3e1098bae410c4ae8c25d092a8787c6c6ff554a9
Tolstov Dmitrij <tolstov.dmitrij42@gmail.com>
2024-11-26 15:29:31 +0400

    icon components

---
+++ src/components/icons/CrownIcons.vue
@@ -0,0 +1,44 @@
+<script setup>
+</script>
+<template>
+	<svg class="crown-draw" xmlns="http://www.w3.org/2000/svg" width="63.289" height="56.381"
+		viewBox="0 0 63.289 56.381">
+		<g id="Group_2866" data-name="Group 2866" transform="translate(-5372.862 1058.614)">
+			<path class="crown" id="Path_10867" data-name="Path 10867"
+				d="M5408.874-1004.047l17.793-31s-15.366,10.556-18.6,10.826c-3.106.259-5.468-8.473-5.649-21.1a.8.8,0,0,0-1.273-.632c-3.2,2.409-11.563,8.694-14.377,10.64-2.711,1.879-7.039-6.417-8.857-10.264a.794.794,0,0,0-1.478.092c-1.3,4.2-2.127,13.98-2.562,21.045a1.57,1.57,0,0,0,1.331,1.653c5.08.766,22.446,4.027,29.739,13.63"
+				stroke-linejoin="round" stroke-width="2" fill='none' stroke='#000'>
+			</path>
+			<path class="c-circle" id="Path_10868" data-name="Path 10868"
+				d="M5407.407-1053.379c1.032,2.284-.172,3.242-1.841,3.242a2.933,2.933,0,0,1-3.1-2.837c0-1.67,2.088-2.836,3.758-2.836"
+				stroke-linejoin="round" stroke-width="2"><svg data-stroke="#fff" xmlns="http://www.w3.org/2000/svg"
+					width="202.007" height="75.327" viewBox="0 0 202.007 75.327">
+					<path class="p-circle" data-name="Path 12270"
+						d="M8196.832-856.3c-96.583-20.014-189.468,22.226-103.276,48.136,82.193,24.708,239.479-1.327,118.643-53.274"
+						stroke-linecap="round" stroke-miterlimit="10" stroke-width="2"
+						data-svg-origin="8059.42333984375 -861.5150756835938">
+					</path>
+				</svg></path>
+			<path class="c-circle" id="Path_10869" data-name="Path 10869"
+				d="M5374.981-1056.093c-.645,1.43.108,2.029,1.154,2.029a1.834,1.834,0,0,0,1.935-1.776c0-1.045-1.306-1.775-2.351-1.775"
+				stroke-linejoin="round" stroke-width="2"><svg data-stroke="#fff" xmlns="http://www.w3.org/2000/svg"
+					width="202.007" height="75.327" viewBox="0 0 202.007 75.327">
+					<path class="p-circle" data-name="Path 12270"
+						d="M8196.832-856.3c-96.583-20.014-189.468,22.226-103.276,48.136,82.193,24.708,239.479-1.327,118.643-53.274"
+						stroke-linecap="round" stroke-miterlimit="10" stroke-width="2"
+						data-svg-origin="8059.42333984375 -861.5150756835938">
+					</path>
+				</svg></path>
+			<path class="c-circle" id="Path_10870" data-name="Path 10870"
+				d="M5431.116-1038.605c-1.773.821-2.529-.111-2.542-1.413a2.287,2.287,0,0,1,2.188-2.434c1.3-.012,2.229,1.608,2.24,2.91"
+				stroke-linejoin="round" stroke-width="2"><svg data-stroke="#fff" xmlns="http://www.w3.org/2000/svg"
+					width="202.007" height="75.327" viewBox="0 0 202.007 75.327">
+					<path class="p-circle" data-name="Path 12270"
+						d="M8196.832-856.3c-96.583-20.014-189.468,22.226-103.276,48.136,82.193,24.708,239.479-1.327,118.643-53.274"
+						stroke-linecap="round" stroke-miterlimit="10" stroke-width="2"
+						data-svg-origin="8059.42333984375 -861.5150756835938">
+					</path>
+				</svg></path>
+		</g>
+	</svg>
+</template>
+<style scoped lang='scss'></style>
