36e80212281ee363ef95917300bc466c73a07c9e
Tolstov Dmitrij <tolstov.dmitrij42@gmail.com>
2024-11-26 13:04:00 +0400

    initializing the project

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+<template>
+	<header class="header">
+		<nav class="header-nav">
+			<img src="" alt="">
+			<ul class="nav-items">
+				<a href="" class="nav-items_item"></a>
+				<a href="" class="nav-items_item"></a>
+				<a href="" class="nav-items_item"></a>
+			</ul>
+		</nav>
+	</header>
+</template>
+<style scoped lang='scss'></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+<template>
+	<h1>hello vue</h1>
+</template>
+<style scoped lang='scss'>
+h1 {
+	font-size: 40px;
+}
+</style>
